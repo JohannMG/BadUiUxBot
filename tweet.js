f9e590ebbbde027eaeaf377d992fb238df3a1373
Johann Garces <hello@johannmg.com>
2016-11-17 19:30:51 -0500

    tweetin code

--- tweet.js
+++ tweet.js
@@ -1,1 +1,18 @@
-console.log(process.env.TESTENVVAR);
+var Twitter = require('twitter');
+var WHOA = require('./DefineAParadigm');
+var generator = new WHOA();
+
+var client = new Twitter({
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token_key: process.env.access_token_key,
+  access_token_secret: process.env.access_token_secret
+});
+
+client.post('statuses/update', {status: generator.getNewInsight() }, function(error, tweet, response) {
+  
+  if (!error) {
+    console.log(tweet);
+  }
+  console.log(response);
+});
